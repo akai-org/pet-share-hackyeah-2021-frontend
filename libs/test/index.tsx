d988b9d51d69aca058d544d2e65a1b22067bcec7
nNown <wojciechmar2000@gmail.com>
2021-12-10 20:44:47 +0100

    Added lib namespace

---
+++ libs/test/index.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from 'react'
+
+type Props = {
+  TestProp: string
+}
+
+const Test: FunctionComponent<Props> = ({ TestProp }) => (
+  <div>
+    <h1>Message from lib component: {TestProp}</h1>
+  </div>
+)
+
+export default Test;
