47065b83e95fc7c9da8ec215be7122fa310fc7bd
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2021-12-10 23:46:59 +0100

    Add props NavBarItem

--- components/ui/NavBar/NavBar.tsx
+++ components/ui/NavBar/NavBar.tsx
@@ -3,7 +3,11 @@ import NavBarItem from "@components/ui/NavBar/NavBarItem/NavBarItem";
 
 const NavBar: FunctionComponent = () => (
   <nav>
-    <NavBarItem>L</NavBarItem>
+    <NavBarItem href="/">Home</NavBarItem>
+    <NavBarItem href="/market/organizations">Organizations</NavBarItem>
+    <NavBarItem href="/user/create">Create</NavBarItem>
+    <NavBarItem href="/market/browse">Items</NavBarItem>
+    <NavBarItem href="/social/messages">Home</NavBarItem>
   </nav>
 )
 
---
+++ pages/user/create.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const Create: NextPage = () => {
+  return (
+    <h1>Create page</h1>
+  )
+}
+
+export default Create;
