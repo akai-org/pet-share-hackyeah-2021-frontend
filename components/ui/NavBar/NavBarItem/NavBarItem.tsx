fc41818c9736f2ad628f81335c477734fe592733
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2021-12-10 23:05:25 +0100

    Add NavBarItem

---
+++ components/ui/NavBar/NavBarItem/NavBarItem.tsx
@@ -0,0 +1,8 @@
+import React, {FunctionComponent} from "react";
+import Link from 'next/link'
+
+const NavBarItem: FunctionComponent = () => (
+  <Link href="/">LINK</Link>
+)
+
+export default NavBarItem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   const { user } = useAppContext();
-    
+
   return (
     <div>
       <h1>{ user ? "true" : "false" }</h1>
