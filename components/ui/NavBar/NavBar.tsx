f71defeb8a385cdf70aa8b5982b648fb19519c9c
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2021-12-11 01:17:48 +0100

    Add Material Bottom Navigation

--- components/ui/NavBar/NavBar.tsx
+++ components/ui/NavBar/NavBar.tsx
@@ -1,14 +1,36 @@
-import React, {FunctionComponent} from 'react';
-import NavBarItem from "@components/ui/NavBar/NavBarItem/NavBarItem";
+import React, {FunctionComponent, useState} from 'react';
+import {BottomNavigation, BottomNavigationAction, BottomNavigationClasses, Box} from "@mui/material";
+import {MdHomeFilled, MdAddCircle, MdOutlineToys, MdOutlineMessage} from "react-icons/md";
+import { GoOrganization } from "react-icons/go";
+import styled from "styled-components";
 
-const NavBar: FunctionComponent = () => (
-  <nav>
-    <NavBarItem href="/">Home</NavBarItem>
-    <NavBarItem href="/market/organizations">Organizations</NavBarItem>
-    <NavBarItem href="/user/create">Create</NavBarItem>
-    <NavBarItem href="/market/browse">Items</NavBarItem>
-    <NavBarItem href="/social/messages">Home</NavBarItem>
-  </nav>
-)
+const NavBox = styled(BottomNavigation)`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  left: 0;
+`
+
+const NavBar: FunctionComponent = () => {
+
+  const [value, setValue] = useState(0)
+
+  return (
+      <NavBox
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}>
+        <BottomNavigationAction label="Home" icon={<MdHomeFilled size="30"/>} value="/"/>
+        <BottomNavigationAction label="Organizations" icon={<GoOrganization size="30"/>} value="/market/organizations"/>
+        <BottomNavigationAction showLabel={false} icon={<MdAddCircle size="40"/>} value="/user/create"/>
+        <BottomNavigationAction label="Items" icon={<MdOutlineToys size="30"/>} value="/market/browse"/>
+        <BottomNavigationAction label="Messages" icon={<MdOutlineMessage size="30"/>} value="/user/organizations"/>
+      </NavBox>
+  )
+}
+
+
 
 export default NavBar;
