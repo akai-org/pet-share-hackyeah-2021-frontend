324af84a574aa8721b32979c465f795c9922dcc9
nNown <wojciechmar2000@gmail.com>
2021-12-11 02:56:14 +0100

    Deleted shit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,4 @@
-/* eslint "react/no-unstable-nested-components":"off" */
-
-import { useState, useRef } from 'react';
-import { SnackbarProvider } from 'notistack';
-import { SessionProvider } from 'next-auth/react';
-import { QueryClient, QueryClientProvider, Hydrate } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { IconButton } from '@mui/material';
-import { FaTimes } from 'react-icons/fa';
 import '../styles/globals.css';
 import { AppProvider } from '@context/AppContext/AppProvider';
 import { Topbar } from '@components/topbar';
@@ -18,42 +9,13 @@ const theme = createTheme({
   },
 });
 
-const MyApp = ({ Component, pageProps }) => {
-  const [queryClient] = useState(() => new QueryClient());
-  const notistackRef = useRef();
-
-  return (
-    <AppProvider>
-      <SessionProvider session={pageProps.session}>
-        <ThemeProvider theme={theme}>
-          <SnackbarProvider
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'center',
-            }}
-            ref={notistackRef}
-            action={(key) => (
-              <IconButton
-                onClick={() => {
-                  notistackRef.current.closeSnackbar(key);
-                }}
-              >
-                <FaTimes color="white" size={20} />
-              </IconButton>
-            )}
-          >
-            <QueryClientProvider client={queryClient}>
-              <Hydrate state={pageProps.dehydratedState}>
-                <Topbar />
-                <Component {...pageProps} />
-                {process.env.NODE_ENV === 'development' && <ReactQueryDevtools />}
-              </Hydrate>
-            </QueryClientProvider>
-          </SnackbarProvider>
-        </ThemeProvider>
-      </SessionProvider>
-    </AppProvider>
-  );
-};
+const MyApp = ({ Component, pageProps }) => (
+  <AppProvider>
+    <ThemeProvider theme={theme}>
+      <Topbar />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </AppProvider>
+);
 
 export default MyApp;
