5b834042c4b7a0ab9596f091f172c1650ae04818
Krychaxp <krychaxp@op.pl>
2021-12-11 03:15:46 +0100

    fixed app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,10 @@
 /* eslint "react/no-unstable-nested-components":"off" */
 
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import { SnackbarProvider } from 'notistack';
 import { SessionProvider } from 'next-auth/react';
 import { QueryClient, QueryClientProvider, Hydrate } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { IconButton } from '@mui/material';
-import { FaTimes } from 'react-icons/fa';
 import '../styles/globals.css';
 
 const theme = createTheme({
@@ -18,31 +15,14 @@ const theme = createTheme({
 
 const MyApp = ({ Component, pageProps }) => {
   const [queryClient] = useState(() => new QueryClient());
-  const notistackRef = useRef();
 
   return (
     <SessionProvider session={pageProps.session}>
       <ThemeProvider theme={theme}>
-        <SnackbarProvider
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-          }}
-          ref={notistackRef}
-          action={(key) => (
-            <IconButton
-              onClick={() => {
-                notistackRef.current.closeSnackbar(key);
-              }}
-            >
-              <FaTimes color="white" size={20} />
-            </IconButton>
-          )}
-        >
+        <SnackbarProvider>
           <QueryClientProvider client={queryClient}>
             <Hydrate state={pageProps.dehydratedState}>
               <Component {...pageProps} />
-              {process.env.NODE_ENV === 'development' && <ReactQueryDevtools />}
             </Hydrate>
           </QueryClientProvider>
         </SnackbarProvider>
