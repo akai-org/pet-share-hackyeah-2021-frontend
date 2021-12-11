332d3826526aa867cc1a434425e676ec7649281d
nNown <wojciechmar2000@gmail.com>
2021-12-11 04:13:15 +0100

    fixed navbar

--- components/ui/NavBar/NavBar.tsx
+++ components/ui/NavBar/NavBar.tsx
@@ -1,39 +1,39 @@
-import React, {FunctionComponent, useState} from 'react';
-import {BottomNavigation, BottomNavigationAction} from "@mui/material";
-import {MdHomeFilled, MdAddCircle, MdOutlineToys, MdOutlineMessage} from "react-icons/md";
-import { GoOrganization } from "react-icons/go";
-import styled from "styled-components";
-import { useRouter } from 'next/router'
-
-const NavBox = styled(BottomNavigation)`
-  position: sticky;
-  bottom: 0;
-  width: 100%;
-  left: 0;
-`
+import React, { FunctionComponent, useState } from 'react';
+import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
+import { MdHomeFilled, MdAddCircle, MdOutlineToys, MdOutlineMessage } from 'react-icons/md';
+import { GoOrganization } from 'react-icons/go';
+import { useRouter } from 'next/router';
 
 const NavBar: FunctionComponent = () => {
-
-  const router = useRouter()
-  const [value, setValue] = useState(0)
+  const router = useRouter();
+  const [value, setValue] = useState(0);
 
   return (
-      <NavBox
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+      <BottomNavigation
         showLabels
         value={value}
         onChange={(event, newValue) => {
           router.push(newValue);
           setValue(newValue);
-        }}>
-        <BottomNavigationAction label="Home" icon={<MdHomeFilled size="30"/>} value="/"/>
-        <BottomNavigationAction label="Organizations" icon={<GoOrganization size="30"/>} value="/market/organizations"/>
-        <BottomNavigationAction showLabel={false} icon={<MdAddCircle size="40" color="var(--secondary)"/>} value="/user/create"/>
-        <BottomNavigationAction label="Items" icon={<MdOutlineToys size="30"/>} value="/market/browse"/>
-        <BottomNavigationAction label="Messages" icon={<MdOutlineMessage size="30"/>} value="/market/organizations"/>
-      </NavBox>
-  )
-}
-
-
+        }}
+      >
+        <BottomNavigationAction label="Home" icon={<MdHomeFilled size="30" />} value="/" />
+        <BottomNavigationAction
+          label="Organizations"
+          icon={<GoOrganization size="30" />}
+          value="/market/organizations"
+        />
+        <BottomNavigationAction
+          showLabel={false}
+          icon={<MdAddCircle size="40" color="var(--secondary)" />}
+          value="/user/create"
+        />
+        <BottomNavigationAction label="Items" icon={<MdOutlineToys size="30" />} value="/market/browse" />
+        <BottomNavigationAction label="Messages" icon={<MdOutlineMessage size="30" />} value="/market/organizations" />
+      </BottomNavigation>
+    </Paper>
+  );
+};
 
 export default NavBar;
