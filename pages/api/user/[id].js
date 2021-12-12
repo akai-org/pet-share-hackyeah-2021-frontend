b24c246884dc9345566598859d1fae9419933aa3
Krychaxp <krychaxp@op.pl>
2021-12-12 02:07:57 +0100

    fixed public user data

--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -1,12 +1,22 @@
 import nc from 'next-connect';
 import { prisma } from 'db';
 
-export default nc()
-  .get(async (req, res) => {
-    const { id } = req.query;
-    const result = await prisma.user.findUnique({
-      where: { id: +id },
-      select: { id: true, username: true, city: true },
-    });
-    res.json(result);
+export default nc().get(async (req, res) => {
+  const { id } = req.query;
+  const result = await prisma.user.findUnique({
+    where: { id: +id },
+    select: {
+      username: true,
+      firstname: true,
+      lastname: true,
+      avatarUrl: true,
+      address: true,
+      phone: true,
+      isActive: true,
+      isOrganization: true,
+      nip: true,
+      rating: true,
+    },
   });
+  res.json(result);
+});
